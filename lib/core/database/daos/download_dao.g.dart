ed65cb2ddc118a5cf448fd24e56740837de041af
Odhiambo Atieno <odhiambosatieno@gmail.com>
2026-08-08 18:25:43 +0300

    chore: regenerate drift code to clear unused-field warning

--- lib/core/database/daos/download_dao.g.dart
+++ lib/core/database/daos/download_dao.g.dart
@@ -4,10 +4,29 @@ part of 'download_dao.dart';
 
 // ignore_for_file: type=lint
 mixin _$DownloadDaoMixin on DatabaseAccessor<AppDatabase> {
+  $FeedingsTable get feedings => attachedDatabase.feedings;
+  $HarvestsTable get harvests => attachedDatabase.harvests;
+  $EggCollectionsTable get eggCollections => attachedDatabase.eggCollections;
+  $CageMaintenancesTable get cageMaintenances =>
+      attachedDatabase.cageMaintenances;
   DownloadDaoManager get managers => DownloadDaoManager(this);
 }
 
 class DownloadDaoManager {
   final _$DownloadDaoMixin _db;
   DownloadDaoManager(this._db);
+  $$FeedingsTableTableManager get feedings =>
+      $$FeedingsTableTableManager(_db.attachedDatabase, _db.feedings);
+  $$HarvestsTableTableManager get harvests =>
+      $$HarvestsTableTableManager(_db.attachedDatabase, _db.harvests);
+  $$EggCollectionsTableTableManager get eggCollections =>
+      $$EggCollectionsTableTableManager(
+        _db.attachedDatabase,
+        _db.eggCollections,
+      );
+  $$CageMaintenancesTableTableManager get cageMaintenances =>
+      $$CageMaintenancesTableTableManager(
+        _db.attachedDatabase,
+        _db.cageMaintenances,
+      );
 }
